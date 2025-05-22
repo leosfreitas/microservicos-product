96c9d481acac20dd769ef62ae661c50c8826b48a
leosfreitas <leonardosf@al.insper.edu.br>
2025-05-22 18:02:23 -0300

    Add initial project structure with .gitignore, Jenkinsfile, and product model classes

---
+++ src/main/java/store/product/ProductIn.java
@@ -0,0 +1,14 @@
+// ProductIn.java
+package store.product;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder @Accessors(fluent = true)
+public record ProductIn(
+    String name,
+    Double price,
+    String unit
+) {
+    
+}
---
+++ src/main/java/store/product/ProductOut.java
@@ -0,0 +1,14 @@
+// ProductOut.java
+package store.product;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder @Accessors(fluent = true)
+public record ProductOut(
+    String id,
+    String name,
+    Double price,
+    String unit
+) {
+}
